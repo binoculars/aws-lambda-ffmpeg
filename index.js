bf4ed6e5b08871505558dbcb3da27d497b025d6b
Barrett K. Harber <barrett.harber@gmail.com>
2015-12-09 01:02:13 -0500

    Config file now requires format for output. config_sample.json shows an example. The default for gzip is false now. PNG and MP4 shouldn't be gzipped as they're already compressed. Got rid of piping to child_process for FFprobe and FFmpeg. Changed default NODE_ENV to production. tempDIr is now either process.env.TEMP (for better Windows support or /tmp Quick fix for gulpfile so it doesn't fail on first run. Bumped AWS SDK to 2.2.12 (current version for Lambda)

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
+process.env['NODE_ENV'] = 'production';
+process.env['PATH'] += ':' + process.env['LAMBDA_TASK_ROOT'];
+
 var child_process = require('child_process');
 var fs = require('fs');
 var util = require('util');
 var zlib = require('zlib');
 var crypto = require('crypto');
 var stream = require('stream');
+var path = require('path');
 var AWS = require('aws-sdk');
 var async = require('async');
-var uuid = require('uuid');
 var config = require('./config');
 var scaleFilter = "scale='min(" + config.videoMaxWidth.toString() + "\\,iw):-2'";
 var s3 = new AWS.S3();
-
-process.env['PATH'] += ':' + process.env['LAMBDA_TASK_ROOT'];
+var tempDir = process.env['TEMP'] || '/tmp';
 
 function downloadStream(bucket, file, cb) {
 	console.log('Starting download');
@@ -32,10 +34,10 @@ function s3upload(params, filename, cb) {
 		.send(cb);
 }
 
-function uploadFile(fileExt, id, bucket, keyPrefix, contentType, cb) {
+function uploadFile(fileExt, bucket, keyPrefix, contentType, cb) {
 	console.log('Uploading', contentType);
 
-	var filename = '/tmp/out.' + fileExt;
+	var filename = path.join(tempDir, 'out.' + fileExt);
 	var rmFiles = [filename];
 	var readStream = fs.createReadStream(filename);
 
@@ -98,106 +100,72 @@ function uploadFile(fileExt, id, bucket, keyPrefix, contentType, cb) {
 }
 
 function ffprobeVerify(cb) {
-	console.log('starting ffprobe');
+	console.log('Starting FFprobe');
 
-	var ffprobe = child_process.execFile(
+	child_process.execFile(
 		'ffprobe',
 		[
 			'-v', 'quiet',
 			'-print_format', 'json',
 			'-show_format',
 			'-show_streams',
-			'-i', '-'
+			'-i', 'download'
 		],
 		{
-			cwd: '/tmp',
-			stdio: [null, null, null, 'pipe']
-		}
-	);
+			cwd: tempDir
+		},
+		function(err, stdout, stderr) {
+			if (err) return cb(err, 'FFprobe failed' + JSON.stringify({ stdout: stdout, stderr: stderr}));
 
-	var output = '';
-
-	ffprobe.stdout.on('data', function (data) {
-		output += data;
-	});
-
-	ffprobe.stdin.on('error', function() {
-		// Handling EPIPE errors. This is a weird, unfixed bug... apparently
-		// https://github.com/joyent/node/issues/7481
-		console.log('ffprobe stdin error');
-	});
-
-	ffprobe.on('exit', function(code, signal) {
-		console.log('ffprobe done');
-
-		if (code)
-			return cb('ffprobe Error code: ' + code + ', signal: ' + signal);
-		else {
-			var outputObj = JSON.parse(output);
+			var outputObj = JSON.parse(stdout);
 			var maxDuration = config.videoMaxDuration;
 
 			var hasVideoStream = outputObj.streams.some(function(stream) {
-				return stream.codec_type === 'video' && stream.duration <= maxDuration;
+				return stream.codec_type === 'video' &&
+						(stream.duration || outputObj.format.duration) <= maxDuration;
 			});
 
 			if (!hasVideoStream)
-				return cb('no valid video stream found');
+				return cb('FFprobe: no valid video stream found');
 			else {
-				console.log('valid video stream found');
-				return cb(null, 'ffprobe finished');
+				console.log('valid video stream found', stdout);
+				return cb(err, 'FFprobe finished');
 			}
 		}
-	});
-
-	return ffprobe;
+	);
 }
 
 function ffmpegProcess(description, cb) {
-	console.log('starting ffmpeg');
+	console.log('Starting FFmpeg');
 
-	var ffmpeg = child_process.execFile(
+	child_process.execFile(
 		'ffmpeg',
 		[
 			'-y',
 			'-loglevel', 'warning',
-			'-i', '-',
+			'-i', 'download',
+			'-c:a', 'copy',
 			'-vf', scaleFilter,
 			'-movflags', '+faststart',
 			'-metadata', 'description=' + description,
-			'out.mp4',
+			'out.' + config.format.video.extension,
 			'-vf', 'thumbnail',
 			'-vf', scaleFilter,
 			'-vframes', '1',
-			'out.png'
+			'out.' + config.format.image.extension
 		],
 		{
-			cwd: '/tmp',
-			stdio: [null, null, null, 'pipe']
+			cwd: tempDir
+		},
+		function(err, stdout, stderr) {
+			console.log('FFmpeg done.');
+			return cb(err, 'FFmpeg finished:' + JSON.stringify({ stdout: stdout, stderr: stderr}));
 		}
 	);
-
-	ffmpeg.on('exit', function (code, signal) {
-		console.log('ffmpeg done');
-
-		if (code)
-			return cb('ffmpeg Error code: ' + code + ', signal: ' + signal);
-		else
-			cb(null, 'ffmpeg finished');
-	});
-
-	return ffmpeg;
 }
 
-function buffer2shorturl(id) {
-	return id
-		.toString('base64')
-		.substring(0, 22)
-		.replace(/\+/g, '-')
-		.replace(/\//g, '_');
-}
-
-function processVideo(s3Event, srcKey, id, cb) {
-	var dlFile = '/tmp/download';
+function processVideo(s3Event, srcKey, cb) {
+	var dlFile = path.join(tempDir, 'download');
 
 	async.series([
 		function(cb) {
@@ -208,10 +176,10 @@ function processVideo(s3Event, srcKey, id, cb) {
 			dlStream.pipe(fs.createWriteStream(dlFile));
 		},
 		function(cb) {
-			fs.createReadStream(dlFile).pipe(ffprobeVerify(cb).stdin);
+			ffprobeVerify(cb);
 		},
 		function(cb) {
-			fs.createReadStream(dlFile).pipe(ffmpegProcess(config.linkPrefix + buffer2shorturl(id), cb).stdin);
+			ffmpegProcess(config.linkPrefix + '/' + srcKey + '.' + config.format.video.extension, cb);
 		},
 		function(cb) {
 			console.log('Deleting download file');
@@ -226,16 +194,15 @@ exports.handler = function(event, context) {
 	var s3Event = event.Records[0].s3;
 	var srcKey = decodeURIComponent(s3Event.object.key);
 	var keyPrefix = srcKey.replace(/\.[^/.]+$/, '');
-	// Key structure on source bucket is 3 folders deep with a UUID filename
-	var id = new Buffer(uuid.parse(keyPrefix.split('/').pop()));
+	var format = config.format;
 
 	async.series([
-		function (cb) { processVideo(s3Event, srcKey, id, cb); },
+		function (cb) { processVideo(s3Event, srcKey, cb); },
 		function (cb) {
 			var dstBucket = config.destinationBucket;
 			async.parallel([
-				function (cb) { uploadFile('mp4', id,  dstBucket, keyPrefix, 'video/mp4', cb); },
-				function (cb) { uploadFile('png', null, dstBucket, keyPrefix, 'image/png', cb); }
+				function (cb) { uploadFile(format.video.extension, dstBucket, keyPrefix, format.video.mimeType, cb); },
+				function (cb) { uploadFile(format.image.extension, dstBucket, keyPrefix, format.image.mimeType, cb); }
 			], cb);
 		}
 	], function(err, results) {
