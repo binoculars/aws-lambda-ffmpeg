26bf7d92b604fb7c12ea86f640efeaed3588d82b
Barrett K. Harber <barrett.harber@gmail.com>
2016-04-21 20:22:16 -0400

    Add documentation

--- platform/common.js
+++ platform/common.js
@@ -6,21 +6,39 @@ const child_process = require('child_process');
 const fs = require('fs');
 const zlib = require('zlib');
 const path = require('path');
+
+/** @type string **/
 const tempDir = process.env['TEMP'] || require('os').tmpdir();
 
-function downloadFile(library, logger, sourceLocation, dlFile) {
+/**
+ * Downloads the file to the local temp directory
+ *
+ * @param {!function} downloadFunc - The platform library's download function
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {{bucket: !string, key: !string}} sourceLocation - The location of the remote file
+ * @param {!string} download - The location of the local file
+ * @returns {Promise}
+ */
+function downloadFile(downloadFunc, logger, sourceLocation, download) {
 	return new Promise((resolve) => {
 		logger.log('Starting download:', sourceLocation.bucket, '/', sourceLocation.key);
 
-		library.getDownloadStream(sourceLocation.bucket, sourceLocation.key)
+		downloadFunc(sourceLocation.bucket, sourceLocation.key)
 			.on('end', () => {
 				logger.log('Download finished');
 				resolve();
 			})
-			.pipe(fs.createWriteStream(dlFile));
+			.pipe(fs.createWriteStream(download));
 	});
 }
 
+/**
+ * Runs FFprobe and ensures that the input file has a valid stream and meets the maximum duration threshold.
+ *
+ * @param {Object} config - The configuration
+ * @param {!{log: !function}} logger - The platform logger
+ * @returns {Promise}
+ */
 function ffprobe(config, logger) {
 	logger.log('Starting FFprobe');
 
@@ -41,12 +59,12 @@ function ffprobe(config, logger) {
 			if (error)
 				reject(error);
 
-			console.log(stdout);
+			logger.log(stdout);
 
-			let outputObj = JSON.parse(stdout);
-			let maxDuration = config.videoMaxDuration;
+			const outputObj = JSON.parse(stdout);
+			const maxDuration = config.videoMaxDuration;
 
-			let hasVideoStream = outputObj.streams.some((stream) => {
+			const hasVideoStream = outputObj.streams.some((stream) => {
 				return stream.codec_type === 'video' &&
 					(stream.duration || outputObj.format.duration) <= maxDuration;
 			});
@@ -65,6 +83,14 @@ function ffprobe(config, logger) {
 	});
 }
 
+/**
+ * Runs the FFmpeg executable
+ *
+ * @param {object} config - The configuration
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {string} keyPrefix - The prefix for the key (filename minus extension)
+ * @returns {Promise}
+ */
 function ffmpeg(config, logger, keyPrefix) {
 	logger.log('Starting FFmpeg');
 
@@ -99,12 +125,29 @@ function ffmpeg(config, logger, keyPrefix) {
 	});
 }
 
-function removeDownload(logger, dlFile) {
+/**
+ * Deletes the download file
+ *
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!string} localFilePath - The location of the local file
+ * @returns {Promise<void>}
+ */
+function removeDownload(logger, localFilePath) {
 	logger.log('Deleting download file');
-	fs.unlinkSync(dlFile);
+	fs.unlinkSync(localFilePath);
+
 	return Promise.resolve();
 }
 
+/**
+ * Encodes the file, if gzip is enabled
+ *
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!string} filename - The filename of the file to encode
+ * @param {boolean} gzip - Whether to GZIP-encode the file, or pass it through
+ * @param {!Array<string>} rmFiles - The files to remove after the operation is complete
+ * @returns {Promise<fs.streamReadable>}
+ */
 function encode(logger, filename, gzip, rmFiles) {
 	return new Promise((resolve) => {
 		const readStream = fs.createReadStream(filename);
@@ -127,12 +170,31 @@ function encode(logger, filename, gzip, rmFiles) {
 	});
 }
 
-function upload(logger, uploadFunc, fileStream, bucket,  key, encoding, mimeType) {
+/**
+ * Uploads the file
+ *
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!function} uploadFunc - The function to upload a processed file
+ * @param {!fs.streamReaddable} - The stream of a processed file
+ * @param {!string} bucket - The remote bucket
+ * @param {!string} key - The remote key/file path
+ * @param {string} encoding - The Content Encoding
+ * @param {string} mimeType - The MIME Type of the file
+ * @returns {Promise}
+ */
+function upload(logger, uploadFunc, fileStream, bucket, key, encoding, mimeType) {
 	logger.log('Uploading', mimeType);
 
 	return uploadFunc(bucket, key, fileStream, encoding, mimeType);
 }
 
+/**
+ * Deletes the local output files
+ * 
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!string} filename - The name of the file
+ * @param {!Array<string>} rmFiles - The files to remove after the operation is complete
+ */
 function removeFiles(logger, filename, rmFiles) {
 	logger.log(filename, 'complete. Deleting now.');
 
@@ -140,7 +202,17 @@ function removeFiles(logger, filename, rmFiles) {
 		.forEach(fs.unlinkSync);
 }
 
-function uploadFile(library, config, logger, keyPrefix, type) {
+/**
+ * Transforms, uploads, and deletes an output file
+ *
+ * @param {!function} uploadFunc - The function to upload a processed file
+ * @param {object} config - The configuration
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!string} keyPrefix - The prefix for the key (filename minus extension)
+ * @param {!string} type - The output file type, as specified in the configuration
+ * @returns {Promise}
+ */
+function uploadFile(uploadFunc, config, logger, keyPrefix, type) {
 	const format = config.format[type];
 	const filename = path.join(tempDir, 'out.' + format.extension);
 	const rmFiles = [filename];
@@ -148,7 +220,7 @@ function uploadFile(library, config, logger, keyPrefix, type) {
 	return encode(logger, filename, config.gzip, rmFiles)
 		.then(fileStream => upload(
 			logger,
-			library.uploadToBucket,
+			uploadFunc,
 			fileStream,
 			config.destinationBucket,
 			keyPrefix + '.' + format.extension,
@@ -158,23 +230,47 @@ function uploadFile(library, config, logger, keyPrefix, type) {
 		.then(() => removeFiles(logger, filename, rmFiles));
 }
 
-function uploadFiles(library, config, logger, keyPrefix) {
+/**
+ * Uploads the output files
+ *
+ * @param {!function} uploadFunc - The function to upload a processed file
+ * @param {object} config - The configuration
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!string} keyPrefix - The prefix for the key (filename minus extension)
+ * @returns {Promise}
+ */
+function uploadFiles(uploadFunc, config, logger, keyPrefix) {
 	return Promise
 		.all(Object.keys(config.format)
-			.map(type => uploadFile(library, config, logger, keyPrefix, type))
+			.map(type => uploadFile(uploadFunc, config, logger, keyPrefix, type))
 		);
 }
 
+/**
+ * The main function
+ *
+ * @param {!{
+ *     getDownloadStream: !function,
+ *     getFileLocation: !function,
+ *     uploadToBucket: !function
+ * }} library - The platform library
+ * @param {object} config - The configuration
+ * @param {!{log: !function}} logger - The platform logger
+ * @param {!{
+ *     event: !object,
+ *     callback: !function
+ * }} invocation - The invocation
+ */
 exports.main = function(library, config, logger, invocation) {
 	const sourceLocation = library.getFileLocation(invocation.event);
 	const keyPrefix = sourceLocation.key.replace(/\.[^/.]+$/, '');
-	const dlFile = path.join(tempDir, 'download');
+	const localFilePath = path.join(tempDir, 'download');
 
-	downloadFile(library, logger, sourceLocation, dlFile)
+	downloadFile(library.getDownloadStream, logger, sourceLocation, localFilePath)
 		.then(() => ffprobe(config, logger))
 		.then(() => ffmpeg(config, logger, keyPrefix))
-		.then(() => removeDownload(logger, dlFile))
-		.then(() => uploadFiles(library, config, logger, keyPrefix))
+		.then(() => removeDownload(logger, localFilePath))
+		.then(() => uploadFiles(library.uploadToBucket, config, logger, keyPrefix))
 		.then(data => invocation.callback())
 		.catch(error => invocation.callback(error));
 };
